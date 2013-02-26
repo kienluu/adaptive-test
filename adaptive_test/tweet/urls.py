3ebe032bf465fbe94187e2389452b5905b0eaada
Kien Luu <kienluu@kien56.local>
2013-02-26 19:15:43 +0000

    fix url match
    Signed-off-by: Kien Luu <kienluu@kien56.local>

--- adaptive_test/tweet/urls.py
+++ adaptive_test/tweet/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from adaptive_test.tweet.views import TweetListView
 
 urlpatterns = patterns('',
-    url(r'', TweetListView.as_view(), name='tweet_tweet_list')
+    url(r'^$', TweetListView.as_view(), name='tweet_tweet_list')
 )
