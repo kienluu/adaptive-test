aacef13d40c2a8a9f40351f310afa50b8d529984
Kien Luu <kienluu@kien56.local>
2013-02-26 19:12:47 +0000

    add tweet detail view
    Signed-off-by: Kien Luu <kienluu@kien56.local>

--- adaptive_test/tweet/views.py
+++ adaptive_test/tweet/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import DetailView
 from django.views.generic.list import ListView
 from adaptive_test.tweet.models import Tweet
 
@@ -5,6 +6,11 @@ from adaptive_test.tweet.models import Tweet
 class TweetListView(ListView):
     model = Tweet
     template_name = 'tweet/tweet_list.html'
+
+
+class TweetView(DetailView):
+    model = Tweet
+    template_name = 'tweet/tweet.html'
 
 
 
