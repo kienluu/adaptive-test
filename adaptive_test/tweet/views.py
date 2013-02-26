8b36f291beb983cf7dfa992eee717a05fd38f46c
Kien Luu <kienluu@kien56.local>
2013-02-26 19:46:21 +0000

    add keyword filtering by get variables to tweet list view
    Signed-off-by: Kien Luu <kienluu@kien56.local>

--- adaptive_test/tweet/views.py
+++ adaptive_test/tweet/views.py
@@ -7,6 +7,16 @@ class TweetListView(ListView):
     model = Tweet
     template_name = 'tweet/tweet_list.html'
 
+    def get_queryset(self):
+        queryset = super(TweetListView, self).get_queryset()
+
+        if self.request.GET:
+            keyword = self.request.GET.get('keyword', None)
+            if keyword:
+                queryset = queryset.filter(message__contains=keyword)
+
+        return queryset.order_by('sentiment')
+
 
 class TweetView(DetailView):
     model = Tweet
