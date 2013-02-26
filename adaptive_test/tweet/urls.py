9e4b4157db2376806d06bdbf1527d52460b23546
Kien Luu <kienluu@kien56.local>
2013-02-26 19:25:13 +0000

    correct tweet view details
    Signed-off-by: Kien Luu <kienluu@kien56.local>

--- adaptive_test/tweet/urls.py
+++ adaptive_test/tweet/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from adaptive_test.tweet.views import TweetListView
+from adaptive_test.tweet.views import TweetListView, TweetView
 
 urlpatterns = patterns('',
-    url(r'^$', TweetListView.as_view(), name='tweet_tweet_list')
+    url(r'^$', TweetListView.as_view(), name='tweet_tweet_list'),
+    url(r'^(?P<pk>[\d]+)/$', TweetView.as_view(), name='tweet_tweet_detail')
 )
